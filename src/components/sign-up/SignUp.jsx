154eb023b158831673c60b369a40507e133b14d2
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-14 22:58:12 -0700

    add firebase auth

--- src/components/sign-up/SignUp.jsx
+++ src/components/sign-up/SignUp.jsx
@@ -11,14 +11,47 @@ import {
 } from "semantic-ui-react";
 // react route
 import { Link } from "react-router-dom";
+// firebase
+import { auth } from "../../firebase/firebase";
 
 export default class extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    };
   }
-  handleChange = () => {};
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password, passwordConfirmation } = this.state;
+    if (password !== passwordConfirmation) {
+      alert("passwords does not match!");
+      return;
+    }
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log("new user is ", user);
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   render() {
+    const { username, email, password, passwordConfirmation } = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -26,7 +59,7 @@ export default class extends Component {
             <Icon name="puzzle piece" color="orange" />
             Register for chat
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handSubmit} size="large">
             <Segment stacked>
               <Form.Input
                 fluid
@@ -36,6 +69,7 @@ export default class extends Component {
                 placeholder="username"
                 onChange={this.handleChange}
                 type="text"
+                value={username}
               />
               <Form.Input
                 fluid
@@ -45,6 +79,7 @@ export default class extends Component {
                 placeholder="Email Address"
                 onChange={this.handleChange}
                 type="email"
+                value={email}
               />
               <Form.Input
                 fluid
@@ -54,6 +89,7 @@ export default class extends Component {
                 placeholder="Password"
                 onChange={this.handleChange}
                 type="password"
+                value={password}
               />
               <Form.Input
                 fluid
@@ -63,6 +99,7 @@ export default class extends Component {
                 placeholder="Confirm Password"
                 onChange={this.handleChange}
                 type="password"
+                value={passwordConfirmation}
               />
               <Button color="orange" fluid size="large">
                 Submit
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,4 +15,6 @@ var firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+export const auth = firebase.auth()
+
 export default firebase;
