68e280b70e6ecb9fdcd13fb941f708a834526a7a
Jeffery Zhang <zhenyu051988@gmail.com>
2020-04-17 00:21:45 -0700

    add message components

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
+//  components
+import MessagesHeader from "../messages-header/MessagesHeader";
+import MessagesForm from "../messages-form/MessagesForm";
+// style
+import { Segment, Comment } from "semantic-ui-react";
 
 export default class Messages extends Component {
   render() {
-    return <div>Messages</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+        <Segment>
+          <Comment.Group className="messages"></Comment.Group>
+        </Segment>
+        <MessagesForm />
+      </React.Fragment>
+    );
   }
 }
